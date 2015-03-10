d2535c8dd23f1154cf1a3352c13d37a9097e620b
jingchun.zhang <jingchun.zhang@code.(none)>
2015-03-10 11:28:00 +0800

    msg

--- network/main.c
+++ network/main.c
@@ -145,6 +145,7 @@ int main(int argc, char **argv) {
 	arg = &(args[i]);
 	snprintf(arg->name, sizeof(arg->name), "./http_server.so");
 	LOG(glogfd, LOG_NORMAL, "prepare start %s\n", arg->name);
+	arg->port = 80;
 	arg->maxevent = myconfig_get_intval("vfs_data_maxevent", 4096);
 	if (init_vfs_thread(arg))
 		goto error;
